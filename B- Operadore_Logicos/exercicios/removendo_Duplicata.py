1431655a2e0ab1ac6682802c27a365b25b2487d7
Junio Roque <jcjunioroque@gmail.com>
2026-09-10 20:36:45 -0300

    Removendo duplicata

--- B- Operadore_Logicos/exercicios/removendo_Duplicata.py
+++ B- Operadore_Logicos/exercicios/removendo_Duplicata.py
@@ -2,8 +2,13 @@
 entrada = input()
 
 # TODO: Crie uma lista com as transações sem duplicatas, mantendo a ordem da primeira ocorrência
-
+lista_transacoes = entrada.split()
+transacoes_unicas = []
 # Dica: Percorra cada transação e adicione à lista apenas se ainda não estiver presente
+
+for transacao in lista_transacoes:
+    if transacao not in transacoes_unicas:
+        transacoes_unicas.append(transacao)
 
 # Descomente após implementar a lógica
 
