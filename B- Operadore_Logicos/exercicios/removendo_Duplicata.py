089ca06304b13618187d8fff1958828b6089da74
Junio Roque <jcjunioroque@gmail.com>
2026-09-10 15:12:01 -0300

    condicional

---
+++ B- Operadore_Logicos/exercicios/removendo_Duplicata.py
@@ -0,0 +1,10 @@
+# Leitura da linha de identificadores de transações
+entrada = input()
+
+# TODO: Crie uma lista com as transações sem duplicatas, mantendo a ordem da primeira ocorrência
+
+# Dica: Percorra cada transação e adicione à lista apenas se ainda não estiver presente
+
+# Descomente após implementar a lógica
+
+print(' '.join(transacoes_unicas))
